ba3c70f18d440bab86377fcf1602ca4380b64f1e
TOHA262000 <m.i.toaha14@gmail.com>
2022-12-02 15:46:20 +0600

    Update my uri

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.04tujxe.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
